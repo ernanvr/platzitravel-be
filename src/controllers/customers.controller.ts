9570d69a6b6ba1b222a1c0c3e850547982612baf
ernanvr <ernanvr@gmail.com>
2022-06-07 22:39:17 -0600

    CHORE: Fixed update Customer's service

--- src/controllers/customers.controller.ts
+++ src/controllers/customers.controller.ts
@@ -12,45 +12,36 @@ import {
 } from '@nestjs/common';
 
 import { Response } from 'express';
+import { CustomersService } from 'src/services/customers.service';
 
 @Controller('customers')
 export class CustomersController {
+  constructor(private customersService: CustomersService) {}
+
   @Get()
   getAll(@Res() response: Response) {
-    response.status(200).send({
-      message: 'array of customers',
-    });
+    response.status(200).send(this.customersService.findAll());
   }
 
   @Get(':id')
   @HttpCode(HttpStatus.ACCEPTED)
-  getId(@Res() response: Response, @Param('id') id: string) {
-    return {
-      message: response,
-      id,
-    };
+  getId(@Res() response: Response, @Param('id') id: number) {
+    const customer = this.customersService.findOne(id);
+    response.send(customer);
   }
 
   @Post()
   create(@Body() payload: any) {
-    return {
-      message: 'action of creating',
-      payload,
-    };
+    return this.customersService.create(payload);
   }
 
   @Put(':id')
   updateCustomer(@Param('id') id: number, @Body() payload: any) {
-    return {
-      id,
-      payload,
-    };
+    return this.customersService.update(id, payload);
   }
 
   @Delete(':id')
   delete(@Param('id') id: number) {
-    return {
-      message: `Customer ${id} has been deleted`,
-    };
+    return this.customersService.delete(id);
   }
 }
--- src/services/customers.service.ts
+++ src/services/customers.service.ts
@@ -3,34 +3,61 @@ import { Customer } from 'src/entities/customer.entity';
 
 @Injectable()
 export class CustomersService {
-  private counterId = 1;
-  private customers: Customer[] = [];
+  private customers: Customer[] = [
+    {
+      id: 1,
+      name: 'Ernan',
+      lastname: 'Velasquez',
+      address:
+        'Pasaje Amapolas, Poligono I, Casa#12, Jardines del Rey, Santa Tecla',
+      phone: '7566-5962',
+      mobile: '-',
+      email: 'ernanvr@gmail.com',
+      details: '',
+      customer_from: '',
+    },
+  ];
+  private counterId = this.customers[this.customers.length - 1].id;
 
   findAll() {
     return this.customers;
   }
 
   findOne(id: number) {
-    return this.customers.find((c) => c.id === id);
+    const response = this.customers.find((c) => c.id === Number(id));
+    return response;
   }
 
   create(payload: any) {
+    this.counterId++;
     const newCustomer = {
       id: this.counterId,
       ...payload,
     };
-    this.counterId++;
     this.customers.push(newCustomer);
     return newCustomer;
   }
 
   update(id: number, payload: any) {
-    let customer = this.findOne(id);
+    const i = this.customers.findIndex((e) => e.id === Number(id));
+    const customer = this.customers[i];
+
     if (customer) {
-      customer = {
+      this.customers[i] = {
+        ...customer,
         ...payload,
       };
     }
-    return customer;
+
+    return this.customers[i];
+  }
+
+  delete(id: number) {
+    const customer = this.findOne(Number(id));
+    if (!customer) {
+      throw new Error(`Id ${id} does not exist`);
+    }
+    const customers = this.customers.filter((customer) => customer.id !== id);
+    this.customers = customers;
   }
 }
