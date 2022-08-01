21ae5a5fbcd56ef91de65ad72e929d0e8bd4667a
ernanvr <ernanvr@gmail.com>
2022-08-01 00:33:41 -0600

    Chore: Transport Company service, dto and entity implemented

--- src/modules/transport-companies/transport-companies.controller.ts
+++ src/modules/transport-companies/transport-companies.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { CreateTransportCompany } from './dtos/transport-companies.dtos';
 import { TransportCompaniesService } from './transport-companies.service';
 
 @Controller('transport-companies')
@@ -8,5 +18,31 @@ export class TransportCompaniesController {
   @Get()
   findAll() {
     return this.transportCompanyService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.transportCompanyService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() payload: CreateTransportCompany) {
+    return this.transportCompanyService.create(payload);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() payload: CreateTransportCompany,
+  ) {
+    return this.transportCompanyService.update(id, payload);
+  }
+
+  @Delete(':id')
+  delete(@Param('id', ParseIntPipe) id: number) {
+    this.transportCompanyService.delete(id);
+    return {
+      message: `Record ${id} was deleted`,
+    };
   }
 }
--- src/modules/transport-companies/transport-companies.service.ts
+++ src/modules/transport-companies/transport-companies.service.ts
@@ -1,6 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import {
+  CreateTransportCompany,
+  UpdateTransportCompany,
+} from './dtos/transport-companies.dtos';
 import { TransportCompany } from './entities/transport-companies.entity';
 
 @Injectable()
@@ -12,5 +16,40 @@ export class TransportCompaniesService {
 
   findAll() {
     return this.transportCompanyRepo.find();
+  }
+
+  findOne(id: number) {
+    const transportCompany = this.transportCompanyRepo.findOne({
+      where: { id },
+    });
+
+    if (!transportCompany) {
+      throw new NotFoundException(`The element with ${id} was not found`);
+    }
+
+    return transportCompany;
+  }
+
+  create(payload: CreateTransportCompany) {
+    const newTransportCompany = this.transportCompanyRepo.create(payload);
+
+    return this.transportCompanyRepo.save(newTransportCompany);
+  }
+
+  async update(id: number, payload: UpdateTransportCompany) {
+    const transportCompany = await this.transportCompanyRepo.findOne({
+      where: { id },
+    });
+    if (!transportCompany) {
+      throw new NotFoundException(`The element with ${id} was not found`);
+    }
+    this.transportCompanyRepo.merge(transportCompany, payload);
+    return this.transportCompanyRepo.save(transportCompany);
+  }
+
+  async delete(id: number) {
+    await this.transportCompanyRepo.delete(id);
+
+    return true;
   }
 }
