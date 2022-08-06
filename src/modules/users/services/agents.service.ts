c61e48aa5e0a8008619409a05196f78d71a80e20
ernanvr <ernanvr@gmail.com>
2022-08-06 10:56:11 -0600

    CHORE: finish agent service

--- src/modules/users/services/agents.service.ts
+++ src/modules/users/services/agents.service.ts
@@ -1,17 +1,18 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository } from 'typeorm';
+import { CreateAgentDto, UpdateAgentDto } from '../dtos/agents.dtos';
 import { Agent } from '../entities/agents.entity';
 
 @Injectable()
 export class AgentsService {
   constructor(@InjectRepository(Agent) private agentRepo: Repository<Agent>) {}
 
-  findAll() {
+  findAll(): Promise<Agent[]> {
     return this.agentRepo.find();
   }
 
-  findOne(id: number) {
+  findOne(id: number): Promise<Agent> {
     const agent = this.agentRepo.findOne({
       where: { id },
     });
@@ -21,5 +22,21 @@ export class AgentsService {
     }
 
     return agent;
+  }
+
+  create(payload: CreateAgentDto): Promise<Agent> {
+    const newAgent = this.agentRepo.create(payload);
+    return this.agentRepo.save(newAgent);
+  }
+
+  async update(id: number, payload: UpdateAgentDto): Promise<Agent> {
+    const agent = await this.findOne(id);
+    this.agentRepo.merge(agent, payload);
+    return this.agentRepo.save(agent);
+  }
+
+  async delete(id: number): Promise<DeleteResult> {
+    await this.findOne(id);
+    return this.agentRepo.delete(id);
   }
 }
