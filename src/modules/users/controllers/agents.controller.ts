cf4eec3e5498116c0b81827e6f66db0c184ef1ee
ernanvr <ernanvr@gmail.com>
2022-08-06 11:24:18 -0600

    CHORE: finish agent controller

--- src/modules/users/controllers/agents.controller.ts
+++ src/modules/users/controllers/agents.controller.ts
@@ -4,37 +4,44 @@ import {
   Delete,
   Get,
   Param,
+  ParseIntPipe,
   Post,
   Put,
 } from '@nestjs/common';
+import { AgentsService } from '../services/agents.service';
+import { Agent } from '../entities/agents.entity';
+import { CreateAgentDto, UpdateAgentDto } from '../dtos/agents.dtos';
+import { DeleteResult } from 'typeorm';
 
 @Controller('agents')
 export class AgentsController {
+  constructor(private agentService: AgentsService) {}
+
   @Get()
-  getAllAgents() {
-    return {};
+  getAllAgents(): Promise<Agent[]> {
+    return this.agentService.findAll();
   }
 
   @Get(':id')
-  getAgent(@Param('id') id: string) {
-    return { id };
+  getAgent(@Param('id', ParseIntPipe) id: number): Promise<Agent> {
+    return this.agentService.findOne(id);
   }
 
   @Post()
-  create(@Body() payload: any) {
-    return {
-      message: 'created',
-      payload,
-    };
+  create(@Body() payload: CreateAgentDto): Promise<Agent> {
+    return this.agentService.create(payload);
   }
 
-  @Put()
-  update(@Param('id') id: string) {
-    return { id };
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() payload: UpdateAgentDto,
+  ): Promise<Agent> {
+    return this.agentService.update(id, payload);
   }
 
-  @Delete()
-  delete(@Param('id') id: string) {
-    return { id };
+  @Delete(':id')
+  delete(@Param('id', ParseIntPipe) id: number): Promise<DeleteResult> {
+    return this.agentService.delete(id);
   }
 }
