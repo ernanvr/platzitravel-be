832e55b301a02e65d992816ad812d78c70eec44d
ernanvr <ernanvr@gmail.com>
2022-08-06 22:04:21 -0600

    CHORE: create room type controller

--- src/modules/hotels/controllers/room-types.controller.ts
+++ src/modules/hotels/controllers/room-types.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Body,
+  Delete,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { DeleteResult } from 'typeorm';
+import { CreateRoomType, UpdateRoomType } from '../dtos/room-types.dtos';
+import { RoomType } from '../entities/room-types.entity';
+import { RoomTypesService } from '../services/room-types.service';
 
 @Controller('room-types')
-export class RoomTypesController {}
+export class RoomTypesController {
+  constructor(private roomTypeService: RoomTypesService) {}
+
+  @Get()
+  getAll(): Promise<RoomType[]> {
+    return this.roomTypeService.findAll();
+  }
+
+  @Get(':id')
+  getOne(@Param('id', ParseIntPipe) id: number): Promise<RoomType> {
+    return this.roomTypeService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() payload: CreateRoomType): Promise<RoomType> {
+    return this.roomTypeService.create(payload);
+  }
+
+  @Put('id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() payload: UpdateRoomType,
+  ): Promise<RoomType> {
+    return this.roomTypeService.update(id, payload);
+  }
+
+  @Delete('id')
+  delete(@Param('id', ParseIntPipe) id: number): Promise<DeleteResult> {
+    return this.roomTypeService.delete(id);
+  }
+}
