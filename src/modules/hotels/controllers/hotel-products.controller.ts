7054e9597f7d42fdb6f5e7614f21df4b11996df8
ernanvr <ernanvr@gmail.com>
2022-08-06 19:38:57 -0600

    CHORE: create hotel product Controller and service

--- src/modules/hotels/controllers/hotel-products.controller.ts
+++ src/modules/hotels/controllers/hotel-products.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Body,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import {
+  CreateHotelProductDto,
+  UpdateHotelProductDto,
+} from '../dtos/hotel-products.dtos';
+import { HotelProduct } from '../entities/hotel-products.entity';
+import { HotelProductsService } from '../services/hotel-products.service';
 
 @Controller('hotel-products')
-export class HotelProductsController {}
+export class HotelProductsController {
+  constructor(private hotelProductService: HotelProductsService) {}
+
+  @Get()
+  getAll(): Promise<HotelProduct[]> {
+    return this.hotelProductService.findAll();
+  }
+
+  @Get(':id')
+  getOne(@Param('id', ParseIntPipe) id: number): Promise<HotelProduct> {
+    return this.hotelProductService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() payload: CreateHotelProductDto) {
+    return this.hotelProductService.create(payload);
+  }
+
+  @Put('id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() payload: UpdateHotelProductDto,
+  ): Promise<HotelProduct> {
+    return this.hotelProductService.update(id, payload);
+  }
+
+  @Delete(':id')
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.hotelProductService.delete(id);
+  }
+}
--- src/modules/hotels/services/hotel-products.service.ts
+++ src/modules/hotels/services/hotel-products.service.ts
@@ -1,4 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, DeleteResult } from 'typeorm';
+import {
+  CreateHotelProductDto,
+  UpdateHotelProductDto,
+} from '../dtos/hotel-products.dtos';
+import { HotelProduct } from '../entities/hotel-products.entity';
 
 @Injectable()
-export class HotelProductsService {}
+export class HotelProductsService {
+  constructor(
+    @InjectRepository(HotelProduct)
+    private hotelProductRepository: Repository<HotelProduct>,
+  ) {}
+
+  findAll(): Promise<HotelProduct[]> {
+    return this.hotelProductRepository.find();
+  }
+
+  findOne(id: number): Promise<HotelProduct> {
+    const hotelProduct = this.hotelProductRepository.findOne({
+      where: { id },
+    });
+
+    if (!hotelProduct) {
+      throw new NotFoundException(`Hotel product ${id} was not found`);
+    }
+
+    return hotelProduct;
+  }
+
+  create(payload: CreateHotelProductDto): Promise<HotelProduct> {
+    const newHotelProduct = this.hotelProductRepository.create(payload);
+    return this.hotelProductRepository.save(newHotelProduct);
+  }
+
+  async update(
+    id: number,
+    payload: UpdateHotelProductDto,
+  ): Promise<HotelProduct> {
+    const hotelProduct = await this.findOne(id);
+    this.hotelProductRepository.merge(hotelProduct, payload);
+    return this.hotelProductRepository.save(hotelProduct);
+  }
+
+  async delete(id: number): Promise<DeleteResult> {
+    await this.findOne(id);
+    return this.hotelProductRepository.delete(id);
+  }
+}
