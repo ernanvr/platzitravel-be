a212f198546ce32ba777beba434e86470a81e2e6
ernanvr <ernanvr@gmail.com>
2022-08-06 19:56:52 -0600

    CHORE: create roomtype service

--- src/modules/hotels/services/room-types.service.ts
+++ src/modules/hotels/services/room-types.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Repository } from 'typeorm';
+import { CreateRoomType, UpdateRoomType } from '../dtos/room-types.dtos';
+import { RoomType } from '../entities/room-types.entity';
 
 @Injectable()
-export class RoomTypesService {}
+export class RoomTypesService {
+  constructor(
+    @InjectRepository(RoomType)
+    private roomTypeRepository: Repository<RoomType>,
+  ) {}
+
+  findAll(): Promise<RoomType[]> {
+    return this.roomTypeRepository.find();
+  }
+
+  async findOne(id: number): Promise<RoomType> {
+    const roomtype = await this.roomTypeRepository.findOne({
+      where: { id },
+    });
+
+    if (!roomtype) {
+      throw new NotFoundException(`Room type ${id} was not found`);
+    }
+
+    return roomtype;
+  }
+
+  create(payload: CreateRoomType): Promise<RoomType> {
+    const newRoomType = this.roomTypeRepository.create(payload);
+    return this.roomTypeRepository.save(newRoomType);
+  }
+
+  async update(id: number, payload: UpdateRoomType): Promise<RoomType> {
+    const roomtype = await this.findOne(id);
+    this.roomTypeRepository.merge(roomtype, payload);
+    return this.roomTypeRepository.save(roomtype);
+  }
+
+  async delete(id: number): Promise<DeleteResult> {
+    await this.findOne(id);
+    return this.roomTypeRepository.delete(id);
+  }
+}
