fdee8e25b328630f2d0f85db18fbcd8ee2e4af1d
ernanvr <ernanvr@gmail.com>
2022-08-06 19:43:13 -0600

    CHORE: add hotel prod & roomtype entities to hotel mod

--- src/modules/hotels/hotels.module.ts
+++ src/modules/hotels/hotels.module.ts
@@ -7,9 +7,11 @@ import { HotelProductsService } from './services/hotel-products.service';
 import { HotelProductsController } from './controllers/hotel-products.controller';
 import { RoomTypesController } from './controllers/room-types.controller';
 import { RoomTypesService } from './services/room-types.service';
+import { HotelProduct } from './entities/hotel-products.entity';
+import { RoomType } from './entities/room-types.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Hotel])],
+  imports: [TypeOrmModule.forFeature([Hotel, HotelProduct, RoomType])],
   providers: [HotelsService, HotelProductsService, RoomTypesService],
   controllers: [HotelsController, HotelProductsController, RoomTypesController],
 })
