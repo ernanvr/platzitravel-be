d58e892f7104412bd842bcf7b23d16c4f3621c21
ernanvr <ernanvr@gmail.com>
2022-08-06 18:08:48 -0600

    CHORE: create room type dtos

---
+++ src/modules/hotels/dtos/room-types.dtos.ts
@@ -0,0 +1,13 @@
+import { IsString, IsNotEmpty, MaxLength } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+
+export class CreateRoomType {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(50, {
+    message: 'Description is too long',
+  })
+  description: string;
+}
+
+export class UpdateRoomType extends PartialType(CreateRoomType) {}
