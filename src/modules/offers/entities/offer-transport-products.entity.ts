7a81ff1f59137b79b068837a68fe5d856460a11b
ernanvr <ernanvr@gmail.com>
2022-08-09 20:33:19 -0600

    Chore: add offer transport product entity

---
+++ src/modules/offers/entities/offer-transport-products.entity.ts
@@ -0,0 +1,62 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class OfferTransportProduct {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    name: 'offer_id',
+  })
+  offerId: number;
+
+  @Column({
+    name: 'transport_product_id',
+  })
+  transportProductId: number;
+
+  @Column({
+    type: 'integer',
+  })
+  quantity: number;
+
+  @Column({
+    name: 'discount_percent',
+    type: 'double',
+    precision: 10,
+    scale: 2,
+    default: 0,
+  })
+  discountPercent: number;
+
+  @Column({
+    name: 'final_product_price',
+    type: 'decimal',
+    precision: 10,
+    scale: 2,
+  })
+  finalProductPrice: number;
+
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  description: string;
+
+  /////////////////////////////
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
